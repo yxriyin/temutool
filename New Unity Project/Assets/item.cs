b819153bc0e71455b52ee45c5b96c24ab68695a2
yxriyin <yxriyin@163.com>
2024-06-01 14:18:55 +0800

    提交

---
+++ New Unity Project/Assets/item.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.IO;
+public class item : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Text No;
+    public Text DZ;
+    public RawImage image; 
+    void Start()
+    {
+        
+    }
+
+    public void clear()
+    {
+        No.text = "";
+        DZ.text = "";
+        image.texture = null;
+    }
+
+    public void setData(string no, string sku, string path)
+    {
+        No.text = "No." + no;
+        DZ.text = sku;
+        Texture2D texture = new Texture2D(300, 300);
+        if(path != "")
+        {
+            byte[] bytes = File.ReadAllBytes(path);
+            texture.LoadImage(bytes);
+            image.texture = texture;
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- New Unity Project/Assets/test.cs
+++ New Unity Project/Assets/test.cs
@@ -11,9 +11,14 @@ public class test : MonoBehaviour
     public Button rename;
     public Button rename1;
     public Button rename2;
+    public Button rename3;
+    public Button rename4;
+    public Text title;
     string chosePath;
+    public Transform Panel;
+    string skcTxtPath;
     // Start is called before the first frame update
-
+    public Camera saveRTCamera;
     public class skcData
     {
         public int skuindex = 6;
@@ -22,18 +27,35 @@ public class test : MonoBehaviour
         public Dictionary<string, int> skcCountDic = new Dictionary<string, int>();
     }
 
+    public class jianhuodanData
+    {
+        public List<List<string>> skuList = new List<List<string>>();
+        public List<string> txtNameList = new List<string>();
+    }
+
     void Start()
     {
+        
         path.onClick.AddListener(PathClick);
         rename.onClick.AddListener(renameClick);
         rename1.onClick.AddListener(renameClick1);
         rename2.onClick.AddListener(renameClick2);
+        rename3.onClick.AddListener(renameClick3);
+        rename4.onClick.AddListener(renameClick4);
         chosePath = getCacheString();
+        skcTxtPath = getCacheTxtPath();
+        Panel.gameObject.SetActive(false);
+        saveRTCamera.enabled = false;
     }
 
     string getCacheString()
     {
         return PlayerPrefs.GetString("path");
+    }
+
+    string getCacheTxtPath()
+    {
+        return PlayerPrefs.GetString("txtpath");
     }
 
     void PathClick()
@@ -54,6 +76,93 @@ public class test : MonoBehaviour
         }
     }
 
+    void renameClick4()
+    {
+        Panel.gameObject.SetActive(true);
+        StartCoroutine(delaySaveImg());
+    }
+
+    void saveImgPng(string txtName, int index)
+    {
+        string name = txtName + "_" + index;
+        saveRTCamera.enabled = true;
+        saveRTCamera.Render();
+        SaveRenderTextureToPNG(name, saveRTCamera.targetTexture, skcTxtPath);
+        saveRTCamera.enabled = false;
+    }
+
+    IEnumerator delaySaveImg()
+    {
+        item[] itemList = Panel.GetComponentsInChildren<item>();
+        jianhuodanData data = filljianhuodanData();
+        List<List<string>> list = data.skuList;
+        clearItem(itemList);
+        int count = 0;
+        int imgCount = 0;
+        for (int i = 0; i < list.Count; i++)
+        {
+            List<string> skulist = list[i];
+            
+            for (int j = 0; j < skulist.Count; j++)
+            {
+                string sku = skulist[j];
+                string path = findPath(sku);
+                int index = j % itemList.Length;
+                itemList[index].setData((j + 1).ToString(), sku, path);
+                count++;
+                if (count == itemList.Length)
+                {
+                    count = 0;
+                    imgCount++;
+                    title.text = data.txtNameList[i] + "_" + imgCount.ToString() + "页";
+                    saveImgPng(data.txtNameList[i], j + 1);
+                    yield return new WaitForSeconds(1);
+                    clearItem(itemList);
+                }
+            }
+            if (count != 0)
+            {
+                count = 0;
+                imgCount++;
+                title.text = data.txtNameList[i] + "_" + imgCount.ToString() + "页";
+                saveImgPng(data.txtNameList[i], skulist.Count);
+            }
+            yield return new WaitForSeconds(1);
+            imgCount = 0;
+            clearItem(itemList);
+        }
+    }
+
+    void clearItem(item[] itemList)
+    {
+        for(int i = 0; i < itemList.Length; i++)
+        {
+            itemList[i].clear();
+        }
+    }
+
+    string findPath(string sku)
+    {
+        string[] files = Directory.GetFiles(chosePath);
+        for(int i = 0; i < files.Length; i++)
+        {
+            if(files[i].IndexOf(sku) >= 0)
+            {
+                return files[i];
+            }
+        }
+
+        return "";
+    }
+
+    void renameClick3()
+    {
+        skcTxtPath = EditorUtility.OpenFolderPanel("Chose Path", skcTxtPath, "");
+        UnityEngine.Debug.Log(skcTxtPath);
+        PlayerPrefs.SetString("txtpath", skcTxtPath);
+        PlayerPrefs.Save();
+
+    }
     void renameClick2()
     {
         createDir("dingzhitupian");
@@ -130,6 +239,44 @@ public class test : MonoBehaviour
                 //
             }
         }
+    }
+
+    jianhuodanData filljianhuodanData()
+    {
+        jianhuodanData data = new jianhuodanData();
+        string[] dirs = Directory.GetDirectories(skcTxtPath);
+
+        string[] csvs = Directory.GetFiles(chosePath, "*.txt");
+        if (csvs.Length < 1)
+        {
+            UnityEngine.Debug.LogError("txt number is not valid:" + csvs.Length);
+            return null;
+        }
+        List<List<string>> skuList = new List<List<string>>();
+
+        List<string> txtNameList = new List<string>();
+        for(int j = 0; j < csvs.Length; j++)
+        {
+            string txtName = Path.GetFileNameWithoutExtension(csvs[j]);
+            txtNameList.Add(txtName);
+            string[] lines = File.ReadAllLines(csvs[j]);
+            List<string> list = new List<string>();
+            for (int i = lines.Length - 1; i >= 0; i--)
+            {
+                if (lines[i].IndexOf("DZ") == 0)
+                {
+                    string[] arr = lines[i].Split(' ');
+                    list.Add(arr[0].Substring(2));
+                }
+            }
+
+            skuList.Add(list);
+        }
+       
+
+        data.skuList = skuList;
+        data.txtNameList = txtNameList;
+        return data;
     }
 
     skcData fillSkcData()
@@ -226,4 +373,24 @@ public class test : MonoBehaviour
             }
         }
     }
+
+    private void SaveRenderTextureToPNG(string textureName, RenderTexture renderTexture, string savePath)
+    {
+        string path = Path.Combine(savePath, textureName + ".png");
+        if (path.Length != 0)
+        {
+            var newTex = new Texture2D(renderTexture.width, renderTexture.height);
+            RenderTexture.active = renderTexture;
+            newTex.ReadPixels(new Rect(0, 0, renderTexture.width, renderTexture.height), 0, 0);
+            newTex.Apply();
+
+            byte[] pngData = newTex.EncodeToPNG();
+            if (pngData != null)
+            {
+                File.WriteAllBytes(path, pngData);
+            }
+            Debug.Log(path);
+        }
+    }
+
 }
